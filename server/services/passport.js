2f680b5cba78ac72bbc2249a0e34764716a0ea0a
ishvar99 <ishanvarshney99@gmail.com>
2021-03-09 03:15:10 +0530

    adding users to database

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,12 +1,25 @@
 const keys=require('../config/keys')
 const passport =require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User=require("../models/user")
 passport.use(new GoogleStrategy({
  clientID: keys.GoogleClientID,
  clientSecret: keys.GoogleClientSecret,
  callbackURL: "/auth/google/callback"
 },
-function(accessToken) {
-console.log(accessToken);
+async (accessToken,refreshToken,profile,done)=>{
+ try{
+ const foundUser= await User.findOne({googleId:profile.id})
+ if(foundUser){
+  done(null,foundUser)
+ }
+ else{
+  const user =await User.create({googleId:profile.id})
+  done(null,user)
+ }
+}catch(err){
+ console.log(err)
 }
+}
+
 ));
